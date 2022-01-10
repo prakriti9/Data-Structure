6ef8d58b7f73f63d242155df79a1d5a92ce31ae6
Prakriti Agrawal <63722780+prakriti9@users.noreply.github.com>
2022-01-10 11:42:51 +0530

    add

---
+++ linkedlist/doubly linked list/insertion.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    int data;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        data = val;
+        prev = NULL;
+        next = NULL;
+    }
+};
+
+void push(Node *head, int data)
+{
+    Node *nptr = new Node(data);
+    
+}
+int main()
+{
+    
+    return 0;
+}
